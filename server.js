ad76794cc4d9fdc7fe93da958ee9c241a98e1a56
Vieldi <61062433+odd1sout-lab@users.noreply.github.com>
2025-05-05 14:27:39 +0600

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: 'root',
+  password: 'SouthParkinlove777$', 
+  database: 'den_sooluk'
 });
 
 connection.connect(err => {
